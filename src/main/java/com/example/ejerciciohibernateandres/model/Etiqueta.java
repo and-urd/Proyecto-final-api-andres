fad06180df81f0675b71f200e8a6a6d135692a13
andresurda@gmail.com <andresurda@gmail.com>
2021-04-14 08:45:05 +0200

    Modelos Etiqueta y Experto

--- src/main/java/com/example/ejerciciohibernateandres/model/Etiqueta.java
+++ src/main/java/com/example/ejerciciohibernateandres/model/Etiqueta.java
@@ -16,4 +16,50 @@ public class Etiqueta {
 
     @ManyToMany(mappedBy = "etiquetas", fetch = FetchType.EAGER)
     private List<Experto> expertos = new ArrayList<>();
+
+
+    // constructores
+    public Etiqueta() {
+    }
+
+    public Etiqueta(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Getter y setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Experto> getExpertos() {
+        return expertos;
+    }
+
+    public void setExpertos(List<Experto> expertos) {
+        this.expertos = expertos;
+    }
+
+    // método toString
+
+    @Override
+    public String toString() {
+        return "Etiqueta{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", num_expertos=" + expertos.size() +
+                '}';
+    }
 }
--- src/main/java/com/example/ejerciciohibernateandres/model/Experto.java
+++ src/main/java/com/example/ejerciciohibernateandres/model/Experto.java
@@ -26,4 +26,68 @@ public class Experto {
     )
     private List<Etiqueta> etiquetas = new ArrayList<>();
 
+
+    // constructores
+    public Experto() {
+    }
+    public Experto(String nombre, String telefono, String email) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    // Getter y setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Etiqueta> getEtiquetas() {
+        return etiquetas;
+    }
+
+    public void setEtiquetas(List<Etiqueta> etiquetas) {
+        this.etiquetas = etiquetas;
+    }
+
+    // método toString
+
+    @Override
+    public String toString() {
+        return "Experto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                ", num_etiquetas=" + etiquetas.size() +
+                '}';
+    }
 }
