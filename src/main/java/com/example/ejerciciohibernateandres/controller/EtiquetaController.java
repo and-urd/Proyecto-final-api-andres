d6ac95e5b2ee21283cd3464c84a2a897141b6197
andresurda@gmail.com <andresurda@gmail.com>
2021-04-16 11:25:35 +0200

    Etiqueta Controller - reordeno los métodos - comento los que no interesan

--- src/main/java/com/example/ejerciciohibernateandres/controller/EtiquetaController.java
+++ src/main/java/com/example/ejerciciohibernateandres/controller/EtiquetaController.java
@@ -4,7 +4,6 @@ import com.example.ejerciciohibernateandres.dao.EtiquetaDAO;
 import com.example.ejerciciohibernateandres.model.Etiqueta;
 import com.example.ejerciciohibernateandres.service.EtiquetaService;
 import org.springframework.data.domain.*;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +23,102 @@ public class EtiquetaController {
         this.etiquetaDAO = etiquetaDAO;
     }
 
+    // Devuelve todas las etiquetas - Filtros: límite , página , nombre
+    @GetMapping("/etiquetas")
+    public Page<Etiqueta> encontrarEtiquetasPorNombrePaginacion(@RequestParam Map<String, String> parametros){
+
+        String nombre;
+        // Si no está el parámetro `nombre` entonces se listan todas las etiquetas.
+        if(parametros.containsKey("nombre")== false){
+            nombre = "";
+        }else{
+            nombre = parametros.get("nombre");
+        }
+
+        // Si no está el parámetro `page` por defecto será 0
+        int page = parametros.containsKey("page")? Integer.parseInt(parametros.get("page")) :0;
+        // Si no está el parámetro `size`, por defecto será 3
+        int size = parametros.containsKey("size")? Integer.parseInt(parametros.get("size")) :10;
+
+        List<Etiqueta> listaEtiqueta = etiquetaDAO.encontrarPorNombre(nombre);
+        if(listaEtiqueta.isEmpty()){
+            return null;
+        }else{
+            //return listaEtiqueta;
+            Pageable pageable =  PageRequest.of(page,size);
+            return convertirListAPage(listaEtiqueta,pageable);
+        }
+    }
+
+    // Devuelve la etiqueta con ID = id
+    @GetMapping("/etiquetas/{id}")
+    public ResponseEntity<Optional<Etiqueta>> encontrarEtiqueta(@PathVariable Long id){
+        Optional<Etiqueta> resultado = etiquetaService.encontrarEtiqueta(id);
+        if(resultado.isPresent()){
+            return ResponseEntity.ok().body(resultado);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Crear una etiqueta
+    @PostMapping("/etiquetas")
+    public ResponseEntity<Etiqueta> crearEtiqueta(@RequestBody Etiqueta etiqueta){
+
+        Etiqueta resultado = etiquetaService.crearEtiqueta(etiqueta);
+
+        if(resultado == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }else{
+            return ResponseEntity.ok().body(resultado);
+        }
+    }
+
+    // Actualiza la etiqueta con ID = id
+    @PutMapping("/etiquetas/{id}")
+    public ResponseEntity<Etiqueta> actualizaEtiquetaPorId(@RequestBody Etiqueta etiqueta, @PathVariable Long id){
+        etiqueta.setId(id);
+        Etiqueta resultado = etiquetaService.actualizarEtiqueta(etiqueta);
+        if(resultado == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }else{
+            return ResponseEntity.ok().body(resultado);
+        }
+    }
+
+    // Borra la etiqueta con ID = id
+    @DeleteMapping("/etiquetas/{id}")
+    public ResponseEntity<Boolean> borrarEtiqueta(@PathVariable Long id){
+
+        Boolean resultado = etiquetaService.borrarEtiqueta(id);
+        if(resultado == true){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Método para convertir una Lista en un objeto Pageable
+    public static <T> Page<T> convertirListAPage(List<T> list, Pageable pageable) {
+        int inicio = (int) pageable.getOffset();
+        int fin = (inicio + pageable.getPageSize()) > list.size() ? list.size() : (inicio + pageable.getPageSize());
+
+        try {
+            Page<T> page = new PageImpl<T>(list.subList(inicio, fin), pageable, list.size());
+            return page;
+        }catch(Exception ex){
+            return null;
+        }
+    }
+
+
+
+
+
+
+
+
+
 
     // Encontrar todas las etiquetas
 //    @GetMapping("/etiquetas")
@@ -38,96 +133,40 @@ public class EtiquetaController {
 
 
     // Encontrar todas las etiquetas CON PAGINACION
-    @GetMapping("/etiquetas")
-    public Page<Etiqueta> encontrarTodasEtiquetasPaginacion(@PageableDefault(size=10, page=0) Pageable pageable){
-        Page<Etiqueta> listaEtiquetas = etiquetaService.encontrarTodas(pageable);
-
-        if(listaEtiquetas.isEmpty()){
-            return null;
-        }else{
-            return listaEtiquetas;
-        }
-    }
-
-
-
-    // Encuentra una etiqueta
-
-    @GetMapping("/etiqueta/{id}")
-    public ResponseEntity<Optional<Etiqueta>> encontrarEtiqueta(@PathVariable Long id){
-        Optional<Etiqueta> resultado = etiquetaService.encontrarEtiqueta(id);
-        if(resultado.isPresent()){
-            return ResponseEntity.ok().body(resultado);
-        }else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    // Crear un etiqueta
-    @PostMapping("/etiqueta")
-    public ResponseEntity<Etiqueta> crearEtiqueta(@RequestBody Etiqueta etiqueta){
-
-        Etiqueta resultado = etiquetaService.crearEtiqueta(etiqueta);
-
-         if(resultado == null){
-             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-         }else{
-             return ResponseEntity.ok().body(resultado);
-         }
-    }
+//    @GetMapping("/etiquetas")
+//    public Page<Etiqueta> encontrarTodasEtiquetasPaginacion(@PageableDefault(size=10, page=0) Pageable pageable){
+//        Page<Etiqueta> listaEtiquetas = etiquetaService.encontrarTodas(pageable);
+//
+//        if(listaEtiquetas.isEmpty()){
+//            return null;
+//        }else{
+//            return listaEtiquetas;
+//        }
+//    }
 
     // Actualiza etiqueta
-    @PutMapping("/etiqueta")
-    public ResponseEntity<Etiqueta> actualizaEtiqueta(@RequestBody Etiqueta etiqueta){
-        Etiqueta resultado = etiquetaService.actualizarEtiqueta(etiqueta);
-        if(etiquetaService == null){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }else{
-            return ResponseEntity.ok().body(resultado);
-        }
-    }
-
-    // Actualizar etiqueta por ID
-    @PutMapping("/etiqueta/{id}")
-    public ResponseEntity<Etiqueta> actualizaEtiquetaPorId(@RequestBody Etiqueta etiqueta, @PathVariable Long id){
-        etiqueta.setId(id);
-        Etiqueta resultado = etiquetaService.actualizarEtiqueta(etiqueta);
-        if(resultado == null){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }else{
-            return ResponseEntity.ok().body(resultado);
-        }
-    }
-
-    // Borrar una etiqueta
-    @DeleteMapping("/etiqueta/{id}")
-    public ResponseEntity<Boolean> borrarEtiqueta(@PathVariable Long id){
-
-        Boolean resultado = etiquetaService.borrarEtiqueta(id);
-        if(resultado == true){
-            return new ResponseEntity<>(HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-
-
-
-
-
+//    @PutMapping("/etiqueta")
+//    public ResponseEntity<Etiqueta> actualizaEtiqueta(@RequestBody Etiqueta etiqueta){
+//        Etiqueta resultado = etiquetaService.actualizarEtiqueta(etiqueta);
+//        if(etiquetaService == null){
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }else{
+//            return ResponseEntity.ok().body(resultado);
+//        }
+//    }
 
     // Crear una lista de etiquetas
-    @PostMapping("/etiqueta/lista")
-    public ResponseEntity<List<Etiqueta>> crearListEtiquetas(@RequestBody List<Etiqueta> etiquetas){
-
-        List<Etiqueta> listaEtiquetas = etiquetaService.crearEtiquetas(etiquetas);
-
-        if(listaEtiquetas.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }else{
-            return ResponseEntity.ok().body(listaEtiquetas);
-        }
-    }
+//    @PostMapping("/etiqueta/lista")
+//    public ResponseEntity<List<Etiqueta>> crearListEtiquetas(@RequestBody List<Etiqueta> etiquetas){
+//
+//        List<Etiqueta> listaEtiquetas = etiquetaService.crearEtiquetas(etiquetas);
+//
+//        if(listaEtiquetas.isEmpty()){
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }else{
+//            return ResponseEntity.ok().body(listaEtiquetas);
+//        }
+//    }
 
     // Recupera las etiquetas por su nombre
 //    @GetMapping("/etiqueta/nombre/{nombre}")
@@ -155,39 +194,5 @@ public class EtiquetaController {
 //            return ResponseEntity.ok().body(listaEtiqueta);
 //        }
 //    }
-
-    // Filtrar por nombre con paginacion
-    @GetMapping("/etiqueta")
-    public Page<Etiqueta> encontrarEtiquetasPorNombrePaginacion(@RequestParam Map<String, String> parametros){
-        if(parametros.containsKey("nombre")== false)
-            return null;
-
-
-        int page = parametros.containsKey("page")? Integer.parseInt(parametros.get("page")) :0;
-        int size = parametros.containsKey("size")? Integer.parseInt(parametros.get("size")) :3;
-
-        String nombre = parametros.get("nombre");
-        List<Etiqueta> listaEtiqueta = etiquetaDAO.encontrarPorNombre(nombre);
-        if(listaEtiqueta.isEmpty()){
-            return null;
-        }else{
-            //return listaEtiqueta;
-            Pageable pageable =  PageRequest.of(page,size);
-            return listConvertToPage1(listaEtiqueta,pageable);
-        }
-    }
-
-    public static <T> Page<T> listConvertToPage1(List<T> list, Pageable pageable) {
-        int start = (int) pageable.getOffset();
-        int end = (start + pageable.getPageSize()) > list.size() ? list.size() : (start + pageable.getPageSize());
-
-        try {
-            Page<T> page = new PageImpl<T>(list.subList(start, end), pageable, list.size());
-            return page;
-        }catch(Exception ex){
-            return null;
-        }
-
-    }
 
 }
